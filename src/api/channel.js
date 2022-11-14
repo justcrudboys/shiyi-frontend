691a726a52abf710bf9931f65e42f4b0977aaa86
unknown <zhzy1108>
2022-11-14 19:50:22 +0800

    Create Channel

--- src/api/channel.js
+++ src/api/channel.js
@@ -9,3 +9,30 @@ export function getCreatorChannel(creator_id) {
     }
   })
 }
+
+export function uploadImg(data) {
+  return request({
+    url: '/api/channel/uploadImg',
+    method: 'post',
+    headers: {
+      'Content-Type': 'multipart/form-data;'
+    },
+    data
+  })
+}
+
+export function createChannel(name, introduction, img, creator_id) {
+  return request({
+    url: '/api/channel/createChannel',
+    method: 'post',
+    data: {
+      name: name,
+      introduction: introduction,
+      img: img,
+      creator_id: creator_id
+    },
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  })
+}

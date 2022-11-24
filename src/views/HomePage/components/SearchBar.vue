6e7a26b071704f39f2c31460ff3b8881baa736ef
VoltChange <prpr23333@163.com>
2022-11-24 16:12:36 +0800

    主页初始项目结构

---
+++ src/views/HomePage/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="searchbar">
+    aaaa
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomePage/components/TagList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="taglist">
+    tttt
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TagList'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
